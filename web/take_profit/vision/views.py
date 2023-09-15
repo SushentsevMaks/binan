28727e927022b94fa97221bcb90d4ad687045801
bibar228 <106908322+bibar228@users.noreply.github.com>
2023-09-15 15:05:03 +0500

    add transaction fee in calculations

--- web/take_profit/vision/views.py
+++ web/take_profit/vision/views.py
@@ -29,14 +29,14 @@ def index(request):
         sum([round(100 * ((float(orders[i].all_cost) / float(orders[i + 1].all_cost)) - 1), 2) for i in
              range(0, len(orders), 2)]), 2)
 
-    percent_change = round(sum([round(100*((float(orders[i].all_cost) / float(orders[i+1].all_cost))-1), 2) for i in range(0, len(orders), 2)]), 2)
+    percent_change = round(sum([round(100*((float(orders[i].all_cost) / float(orders[i+1].all_cost))-1.0015), 2) for i in range(0, len(orders), 2)]), 2)
     percent_change_for_day = {}
     for d in sorted(set([i.time[:10] for i in orders])):
         for j in orders:
             if d == j.time[:10]:
-                percent_change_for_day[d] = round(sum([round(100*((float(orders[i].all_cost) / float(orders[i+1].all_cost))-1), 2) for i in range(0, len(orders), 2) if orders[i].time[:10] == d]), 2)
+                percent_change_for_day[d] = round(sum([round(100*((float(orders[i].all_cost) / float(orders[i+1].all_cost))-1.0015), 2) for i in range(0, len(orders), 2) if orders[i].time[:10] == d]), 2)
 
     d = 0
     for i in range(len(sells)):
-        d += float(sells[i].all_cost) - float(buys[i].all_cost)
+        d += float(sells[i].all_cost) - float(buys[i].all_cost) - ((float(sells[i].all_cost) + float(buys[i].all_cost)) / 100) * 0.15
     return render(request, "vision/index.html", {"orders": orders, "buys": round(d, 2), "percent_change": percent_change, "percent_change_for_day": percent_change_for_day})
